50cde6dc86be8a96c0a88f46c416b5817a22dbf5
Alex Tsang <atsang1117@hotmail.com>
2016-07-21 12:17:27 -0700

    redoing the structure

---
+++ server.js
@@ -0,0 +1,27 @@
+//server.js
+
+
+//moduels
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+
+//configuration
+
+//config files
+var dp = reqiore(./config/db);
+
+//set our port
+var port = process.env.PORT || 8080;
+
+//connecto to our mongoose db
+//uncomment when you ahve your db credentials in config/db.js
+//mongoose.connect(db.url);
+
+
+//get all data of the body (POST) parameters
+//parse body application
+app.use(bodyParser.json());
+
+//
