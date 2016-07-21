768f12b3b4004658adec6e74278a167918f26302
Alex Tsang <atsang1117@hotmail.com>
2016-07-21 14:57:02 -0700

    structure done. need to fill

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,18 @@
 var User = require("./models/user");
 
 module.exports = function(app){
-    
-}
+    app.get('/api/budget', function(req, res) {
+        User.find(function(err, users) {
+
+
+            if (err)   
+                res.send(err);
+            res.json(users);
+        });
+    });
+
+    app.get("*", function(req, res) {
+        res.sendfile('./public/views/index.html');
+    });
+
+};
